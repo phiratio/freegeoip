6334ff2ec1fbf4babc7c1f0d438b8aa014c38a5c
phiratio <phiratio161@gmail.com>
2020-08-25 15:13:48 +0300

    db file

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p /tmp/freegeoip
-
-[ -f "/db.gz" ] && cp /db.gz /tmp/freegeoip/db.gz
-
-exec /go/bin/freegeoip
--- go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/apilayer/freegeoip
-
-go 1.13
-
-require (
-	github.com/prometheus/client_golang v0.9.2
-)
